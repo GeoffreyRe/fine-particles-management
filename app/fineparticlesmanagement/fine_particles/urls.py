6c448f925395a1f92a3d745613d33b2211f96332
Geoffrey REmacle <geoffreyremacle@outlook.fr>
2025-05-09 14:42:20 +0200

    list view improvement

--- app/fineparticlesmanagement/fine_particles/urls.py
+++ app/fineparticlesmanagement/fine_particles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import MeasurementListView
+from .views import MeasurementListView, MeasurementByTypeListView
 
 urlpatterns = [
     path('', MeasurementListView.as_view(), name='measurement_list'),
+    path('type/<str:type_name>/', MeasurementByTypeListView.as_view(), name='measurement_by_type'),
 ]
--- app/fineparticlesmanagement/fine_particles/views.py
+++ app/fineparticlesmanagement/fine_particles/views.py
@@ -1,7 +1,28 @@
 from django.views.generic import ListView
-from .models import Measurement
+from .models import Measurement, MeasurementType
 
 class MeasurementListView(ListView):
     model = Measurement
     template_name = 'fine_particles/measurement_list.html'
     context_object_name = 'measurements'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['types'] = MeasurementType.objects.all()
+        return context
+
+
+class MeasurementByTypeListView(ListView):
+    model = Measurement
+    template_name = 'fine_particles/measurement_list.html'
+    context_object_name = 'measurements'
+
+    def get_queryset(self):
+        type_name = self.kwargs['type_name']
+        return Measurement.objects.select_related('unit', 'type').filter(type__name=type_name)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter_type'] = self.kwargs['type_name']
+        context['types'] = MeasurementType.objects.all()
+        return context
