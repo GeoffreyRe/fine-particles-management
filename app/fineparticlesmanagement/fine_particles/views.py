5dd93f182e116ab987f69f743012fa1d040c128a
Mas_destructor <93466036+Masdestructor@users.noreply.github.com>
2025-05-14 19:36:50 +0200

    Revert "add Paginator"
    This reverts commit 67d1052bc14b082532c3d08e84f5548bd8fcbdcb.

--- app/fineparticlesmanagement/fine_particles/views.py
+++ app/fineparticlesmanagement/fine_particles/views.py
@@ -1,39 +1,24 @@
-from django.core.paginator import Paginator
 from django.views.generic import ListView
 from .models import Measurement, MeasurementType
 
 class MeasurementListView(ListView):
     model = Measurement
     template_name = 'fine_particles/measurement_list.html'
-    context_object_name = 'measurements'  
-    paginate_by = 10  
+    context_object_name = 'measurements'
 
     def get_queryset(self):
-        filter_type = self.request.GET.get('type_name', None)
-        if filter_type:
-            return Measurement.objects.filter(type__name=filter_type).order_by('-time', 'type')
-        else:
-            return Measurement.objects.all().order_by('-time', 'type')
+        return Measurement.objects.all().order_by('-time', 'type')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        
-        measurements_list = self.get_queryset()
-        paginator = Paginator(measurements_list, self.paginate_by) 
-        page_number = self.request.GET.get('page') 
-        page_obj = paginator.get_page(page_number) 
-        
-        context['measurements'] = page_obj  
-        context['types'] = MeasurementType.objects.all() 
-        context['filter_type'] = self.request.GET.get('type_name', None) 
-        context['is_paginated'] = page_obj.has_other_pages()
+        context['types'] = MeasurementType.objects.all()
         return context
+
 
 class MeasurementByTypeListView(ListView):
     model = Measurement
     template_name = 'fine_particles/measurement_list.html'
     context_object_name = 'measurements'
-    paginate_by = 10
 
     def get_queryset(self):
         type_name = self.kwargs['type_name']
@@ -41,14 +26,6 @@ class MeasurementByTypeListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
-        measurements_list = self.get_queryset()
-        paginator = Paginator(measurements_list, self.paginate_by) 
-        page_number = self.request.GET.get('page') 
-        page_obj = paginator.get_page(page_number) 
-
-        context['measurements'] = page_obj
         context['filter_type'] = self.kwargs['type_name']
         context['types'] = MeasurementType.objects.all()
-        context['is_paginated'] = page_obj.has_other_pages()
         return context
